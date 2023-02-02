0516ceae5f5b04ca8f69cecdf68e3669b0963bfa
Florentin LEPELTIER <florentin.lepeltier@reseau.eseo.fr>
2023-02-02 10:19:02 +0100

    Init commit

---
+++ src/package/exemple.c
@@ -0,0 +1,9 @@
+extern int fct(int p)
+{
+	return p+1;
+}
+
+static int fct_static(int p)
+{
+	return p+1;
+}
---
+++ src/package/main.c
@@ -0,0 +1,9 @@
+#define UNE_INFO (42)
+//OUAIS LE COMMENTAIRE
+#include <stdio.h>
+
+extern int fct(int p);
+
+int main(){
+	return fct(UNE_INFO);
+}
